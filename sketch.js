67f0c0f039c06ff1587e7a0f0d627b14d25ef832
DiogoAlex03 <128501698+DiogoAlex03@users.noreply.github.com>
2023-03-21 17:21:19 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+function setup() {
+	createCanvas(800, 784);
+
+}
+
+function draw() {
+
+	background(255);
+	noStroke();
+	fill(238,36,0);
+	rect(0,200,300,400);
+	fill(28,21,105);
+	rect(300,100,400,150);
+	stroke(30);
+	strokeWeight(random(15,20));
+	line(0,250,width,250);
+	strokeWeight(random(23,26));
+	line(0,100,width,100);
+	strokeWeight(random(12,17));
+	line(300,0,300,height); 
+	line(0,600,width,600);
+	strokeWeight(random(20,23));
+	line(0,200,width,200);
+	strokeWeight(random(10,15));
+	line(300,0,300,height); }
